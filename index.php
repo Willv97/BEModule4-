ff2dfc1a33a30aa26a8c46d8b1483d28ab9c07b6
Willv97 <willv1357@gmail.com>
2023-06-27 00:14:18 -0400

    Updated code to add missing requirements

--- index.php
+++ index.php
@@ -1,74 +1,97 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>TestPhPPage</title>
-        <link rel="stylesheet" href="main.css">
-    </head>
-    <body>
-        <header>Module4-2</header>
-        <h1>Family Relations</h1>
-        <?php
-        define("Family", "My Family"); // a named constant
+<head>
+    <title>TestPhPPage</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <header>Module4-2</header>
+    <h1>Family Relations</h1>
+    <?php
+    define('Family', 'My Family'); // Named constant
 
-        $name1 = "will"; // Define a string variable called name1
-        $age1 = "25"; // Define an integer variable called age1
-        $likesMusic1 = false; // Define a boolean variable called $likesMusic1
+    $people = array();
 
-        $name2 = "chuk"; // Define a string variable called name2
-        $age2 = "13"; // Define an integer variable called age2
-        $likesMusic2 = true; // Define a boolean variable called $likesMusic2
-
-        $name3 = "renzo"; // Define a string variable called name3
-        $age3 = "10"; // Define an integer variable called age3
-        $likesMusic3 = true; // Define a boolean variable called $likesMusic3
-
-        $name4 = "lily"; // Define a string variable called name3
-        $age4 = "8"; // Define an integer variable called age3
-        $likesMusic4 = false; // Define a boolean variable called $likesMusic3
-
-        $name5 = "egypt"; // Define a string variable called name3
-        $age5 = "1"; // Define an integer variable called age3
-        $likesMusic5 = false; // Define a boolean variable called $likesMusic3
-        
-        // Multi-dimensional array
-        $people = array(
-            array("name" => "will", "age" => 25, "likesMusic" => false),
-            array("name" => "chuk", "age" => 13, "likesMusic" => true),
-            array("name" => "renzo", "age" => 10, "likesMusic" => true),
-            array("name" => "lily", "age" => 8, "likesMusic" => false),
-            array("name" => "egypt", "age" => 1, "likesMusic" => false)
+    // Function to add a person to the array
+    function addPerson($name, $age, $likesMusic, $favoriteArtist) {
+        global $people;
+        $person = array(
+            "name" => $name, // string variable
+            "age" => $age,
+            "likesMusic" => $likesMusic,
+            "favoriteArtist" => $favoriteArtist // string variable
         );
+        $people[] = $person;
+    }
 
-        // Iterated and accessed one-dimensional array
-        $fruits = array("Apple", "Banana", "Orange");
-        for ($i = 0; $i < count($fruits); $i++) {
-            echo "Fruit " . ($i + 1) . ": " . $fruits[$i] . "<br>";
+    // Add people to the array based on user input
+    addPerson("will", 25, false, "");
+    addPerson("chuk", 13, true, "");
+    addPerson("renzo", 10, true, "");
+    addPerson("lily", 8, false, "");
+    addPerson("egypt", 1, false, "");
+
+    define('familymembers', count($people)); // Named constant
+
+    // For loop that displays the favorite artist for each person
+    foreach ($people as $person) {
+        if (empty($person["favoriteArtist"])) {
+            echo "No favorite artist specified for " . $person["name"] . ".<br>";
+        } else {
+            echo "The favorite artist of " . $person["name"] . " is: " . $person["favoriteArtist"] . "<br>";
         }
+    }
 
-        // While loop
-        $counter = 1;
-        while ($counter <= 5) {
-            echo "Counter: " . $counter . "<br>";
-            $counter++;
-        }
+    // Process the form submission
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Retrieve favorite artists from the form
+        $favoriteArtists = $_POST["favoriteArtists"];
 
-        // Called function
-        function printPersonInfo($person) {
-            echo "My name is " . $person["name"] . ", I am " . $person["age"] . " years old";
-
-            if ($person["likesMusic"]) {
-                echo " and I like music!<br>";
-            } else {
-                echo " and I don't like music.<br>";
-            }
-        }
-
-        // Call the function for each person
+        // Update the favorite artists for each person
         for ($i = 0; $i < count($people); $i++) {
-            echo "Person " . ($i + 1) . ": ";
-            printPersonInfo($people[$i]);
+            $people[$i]["favoriteArtist"] = $favoriteArtists[$i];
         }
 
-        ?>
-    </body>
+        // Display the favorite artists entered by the user
+        foreach ($people as $person) {
+            echo "Favorite artist for " . $person["name"] . ": " . $person["favoriteArtist"] . "<br>";
+        }
+    }
+    ?>
+
+    <!-- HTML form to enter favorite artists -->
+    <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+        <?php foreach ($people as $person): ?>
+            <label for="<?php echo $person["name"]; ?>">Favorite artist for <?php echo $person["name"]; ?>:</label>
+            <input type="text" name="favoriteArtists[]" id="<?php echo $person["name"]; ?>"><br>
+        <?php endforeach; ?>
+        <input type="submit" value="Submit">
+    </form>
+
+    <?php
+    // While loop
+    $counter = 1;
+    while ($counter <= count($people)) {
+        echo "Counter: " . $counter . "<br>";
+        $counter++;
+    }
+
+    // Called function
+    function printPersonInfo($person) {
+        echo "My name is " . $person["name"] . ", I am " . $person["age"] . " years old";
+
+        if ($person["likesMusic"]) {
+            echo " and I like music!<br>";
+        } else {
+            echo " and I don't like music.<br>";
+        }
+    }
+
+    // Call the function for each person
+    foreach ($people as $person) {
+        echo "Person: ";
+        printPersonInfo($person);
+    }
+    ?>
+</body>
 </html>
